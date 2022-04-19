7fa754ae89d20b1c3faebaa80456adff105a0327
Kyohei Hamaguchi (@tnzk) <tnzk@users.noreply.github.com>
2022-04-20 01:49:50 +0900

    Maybe preview build won't repro

--- src/prisma.ts
+++ src/prisma.ts
@@ -2,5 +2,13 @@ import type { PrismaClient } from '@prisma/client'
 import Prisma from '@prisma/client'
 
 export let prisma: PrismaClient
-console.debug('Default export is there!');
-prisma = new Prisma.PrismaClient()
+
+if (Prisma === undefined) {
+  console.debug('Default export is undefined');
+  import('@prisma/client').then(({ PrismaClient }) => {
+    prisma = new PrismaClient()
+  })
+} else {
+  console.debug('Default export is there!');
+  prisma = new Prisma.PrismaClient()
+}
