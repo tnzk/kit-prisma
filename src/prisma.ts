1e7092e1ad3d99c86a746c067759215821297ed7
Kyohei Hamaguchi (@tnzk) <tnzk@users.noreply.github.com>
2022-04-20 01:40:15 +0900

    Using Prisma namespace works?

--- src/prisma.ts
+++ src/prisma.ts
@@ -2,15 +2,5 @@ import type { PrismaClient } from '@prisma/client'
 import Prisma from '@prisma/client'
 
 export let prisma: PrismaClient
-
-/*
-if (Prisma === undefined) {
-  console.debug('Default export is undefined');
-  import('@prisma/client').then(({ PrismaClient }) => {
-    prisma = new PrismaClient()
-  })
-} else {
-  */
-  console.debug('Default export is there!');
-  prisma = new Prisma.PrismaClient()
-//}
+console.debug('Default export is there!');
+prisma = new Prisma.PrismaClient()
--- src/routes/data.json.ts
+++ src/routes/data.json.ts
@@ -1,11 +1,12 @@
-import type { Role } from "@prisma/client";
+import Prisma from "@prisma/client";
+import type { Role } from '@prisma/client'
 import { prisma } from "../prisma";
 
 /** @type {import('./data.json').RequestHandler} */
 export async function get({ }) {
   const created = await prisma.user.create({ data: { name: 'some name' } });
 
-  const r:Role|undefined = undefined;
+  const r:Role|undefined = Prisma.Role.ADMIN;
   console.debug('Test:', r);
 
   if (created) {
