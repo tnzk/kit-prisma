c40049e64f40635d9c1663fc44b5ebbd155d4bf4
Kyohei Hamaguchi (@tnzk) <tnzk@users.noreply.github.com>
2022-04-20 12:36:44 +0900

    Using enum as values and as a type

--- src/routes/data.json.ts
+++ src/routes/data.json.ts
@@ -1,12 +1,13 @@
-import type { Role } from '@prisma/client'
+import Prisma from '@prisma/client'
 import { prisma } from "../prisma";
 
 /** @type {import('./data.json').RequestHandler} */
 export async function get({ }) {
   const created = await prisma.user.create({ data: { name: 'some name' } });
 
-  const r:Role|undefined = undefined;
+  const r:Prisma.Role|undefined = Prisma.Role.ADMIN;
   console.debug(`Test at ${process.env.VERCEL_GIT_COMMIT_SHA ?? 'develop/preview'}`);
+  console.debug(`Using Enum both as values and as a type: ${r}`)
 
   if (created) {
     return {
